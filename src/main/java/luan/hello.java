97bfbda3e3753c95e7dc80fb1bd2386f596c2b22
vuluan90 <vuvanluan90@gmail.com>
2018-10-19 21:12:11 +0700

    1
    1

---
+++ src/main/java/luan/hello.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class hello {
+
+    @RequestMapping("/")
+    public String viewtrangchu() {
+    
+        return ("index");
+    }
+     @RequestMapping("/trangchu")
+    public String viewtrangchu1() {
+    
+        return ("trangchu");
+    }
+
+}
